9021464d6d619bbe8ed46f8eef5c0812416f3391
Lennart Kats <l.c.l.kats add tudelft.nl@localhost>
2008-11-19 12:05:17 +0100

    Added a basic benchmark application.

---
+++ src/kiama/example/picojava/benchmark/PicoJavaBenchmark.scala
@@ -0,0 +1,49 @@
+package kiama.example.picojava.benchmark
+
+import kiama.attribution._
+import kiama.example.picojava.tests._
+import kiama.example.picojava.AbstractSyntax._
+import kiama.example.picojava.ErrorCheck._
+
+object PicoJavaBenchmark extends Application {
+    // For the actual program text this is based on, see DotNameResolutionTests.pj
+    
+    def basicAst = ClassDecl ("AA", None, Block (List (VarDecl ("x", Use ("int")))))
+    
+    def createAst (subtree : ClassDecl) =
+        ClassDecl ("AA", None, Block (
+              List (VarDecl ("y", Use ("int")),
+                    VarDecl ("a", Use ("AA")),
+                    AssignStmt (Use ("x"),
+                                Dot (Use ("a"), Use ("x"))),
+                    subtree,
+                    ClassDecl ("BB", Some (Use ("AA")), Block (
+                        List (VarDecl ("b", Use ("BB")),
+                              AssignStmt (Dot (Use ("b"), Use("y")),
+                                          Dot (Use ("b"), Use("x")))))))))
+    
+    def createProgram (subtree : ClassDecl) =
+        Program (Block (List (subtree)))
+    
+    // Warm up the JIT compiler
+    
+    for (i <- 0 to 10000) {
+        val result = createProgram(createAst(basicAst))->errors
+        // print(result.size)
+        Attribution.resetMemo
+    }
+    
+	val start = System.currentTimeMillis
+	for (i <- 0 to 10) {
+	    // Create a big AST
+	    
+	    var bigAsst = createAst(basicAst)
+	    for (i <- 0 to 200) bigAsst = createAst(bigAsst)
+	    
+	    // Evaluate some attributes
+	    createProgram(bigAsst)->errors
+	    
+	    Attribution.resetMemo
+	}
+	println(System.currentTimeMillis - start)
+}
