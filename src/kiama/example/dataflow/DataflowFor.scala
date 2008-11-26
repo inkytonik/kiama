4de13d1c31a845678de2c0b16ef1ccf404c84623
Lennart Kats <l.c.l.kats@tudelft.nl>
2008-11-26 17:23:08 +0100

    formatting

--- src/kiama/example/dataflow/DataflowFor.scala
+++ src/kiama/example/dataflow/DataflowFor.scala
@@ -8,12 +8,12 @@ case class Foreach (cond : Var, body : Stm) extends Stm
 object DataflowForeach {
     
     Dataflow.succ +=
-        attr { case t @ Foreach (_, body) => t->following + body }
+        attr { case t @ Foreach (_, body) => following (t) + body }
     
     // Using the childAttr notation
     Dataflow.following += 
         childAttr {
-            _ => { case t @ Foreach(_, body) => following(t) + body }
+            _ => { case t @ Foreach(_, body) => following (t) + body }
         }
     
     // Alternatively, using the regular attr notation
@@ -21,7 +21,7 @@ object DataflowForeach {
         attr {
             case t if t.parent.isInstanceOf[Foreach] =>
                  val parent = t.parent[Foreach]
-                 following(parent) + parent.body
+                 following (parent) + parent.body
         }
 }
 
@@ -36,7 +36,7 @@ object DataflowFor {
         childAttr {
             S => {
                 case t @ For (S, c, _, _) => Set (c)
-                case t @ For (_, S, _, b) => following(t) + b
+                case t @ For (_, S, _, b) => following (t) + b
                 case t @ For (_, c, S, _) => Set (c)
                 case t @ For (_, _, i, S) => Set (i)
             }
