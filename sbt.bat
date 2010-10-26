c286b303e51b0da6acc21f6c50d05877f5282cac
Tony Sloane <inkytonik@gmail.com>
2010-10-26 14:17:03 +1100

    Make Java options for sbt.bat the same as sbt script

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java -Xmx512M -Xss2M -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
+java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -Xss2M -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
