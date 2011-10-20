3d667351dd66365400439e181e19fe3b0a299eed
Tony Sloane <inkytonik@gmail.com>
2011-10-20 15:30:14 +1100

    Reuse default prompt in lambda2

--- src/org/kiama/example/lambda2/Lambda.scala
+++ src/org/kiama/example/lambda2/Lambda.scala
@@ -44,7 +44,7 @@ object Lambda extends ParsingREPL[AST.Exp] with Parser {
         true
     }
 
-    override def prompt () = "lambda2> "
+    override def prompt () = mechanism + super.prompt
 
     /**
      * Are we currently type-checking or not?  Default: true.
