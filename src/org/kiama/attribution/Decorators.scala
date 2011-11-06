d2b2239eb7d53b6433344f9c7d22536851d3ee88
Tony Sloane <inkytonik@gmail.com>
2011-11-07 10:28:10 +1100

    Generalise chain decorator so we can now update on both in and out

--- src/org/kiama/attribution/Decorators.scala
+++ src/org/kiama/attribution/Decorators.scala
@@ -39,56 +39,70 @@ object Decorators {
         }
 
     /**
-     * A decorator that propagates an attribute value in left-to-right postorder
-     * through a tree.  init is a partial function that provides initial values
-     * for the chain at selected nodes.  update is a function that transforms
-     * a default function for the chain value into a partial function that
-     * provides an updated value at selected nodes.
-     * 
-     * chain returns a pair of attributes that can be used to access the value
-     * of the chain as it enters (resp. leaves) any node.
+     * A pair of attributes that thread through a tree in a depth-first
+     * left-to-right fashion.  The in (out) attributes provides the value
+     * of the chain as it enters a node from above (leaves a node from 
+     * below).  The chain as a function is the same as the out attribute.
      */
-    def chain[T <: Attributable,U] (init : T ==> U) (update : (T => U) => (T ==> U)) :
-            (T => U, T => U) = {
+    case class Chain[T,U] (in : (T => U), out : (T => U)) extends (T => U) {
+        def apply (t : T) = out (t)
+    }
+
+    /**
+     * An identity function for chain updates.
+     */
+    private def idf[T,U] : (T => U) => (T ==> U) =
+        f => { case t => f (t) }
+
+    /**
+     * Create a new attribute chain.  The update functions provide ways to 
+     * influence the chain value, by taking the default computation of the
+     * in or out attribute and returning a partial function.  If the domain
+     * of the partial function contains a node, then that function is used
+     * to compute the chain value at the node n, otherwise the default chain
+     * attribute is used.  If an update function is omitted, it defaults 
+     * to the identity.
+     */
+    def chain[T <: Attributable,U] (
+                 inupdate : (T => U) => (T ==> U) = idf,
+                 outupdate : (T => U) => (T ==> U) = idf
+             ) : Chain[T,U] = {
+
+        def update (dflt : T => U, upd : (T => U) => (T ==> U)) : T => U =
+            attr {
+                case t =>
+                    val f = upd (dflt)
+                    if (f.isDefinedAt (t))
+                        f (t)
+                    else
+                        dflt (t)
+            }
 
         lazy val indflt : T => U =
             attr {
+                case t if t.isRoot =>
+                    sys.error ("chain indflt: root of tree reached at " + t)
                 case t if t.isFirst =>
-                    (t.parent[T])->inattr
+                    (t.parent[T])->in
                 case t =>
-                    (t.prev[T])->outattr
+                    (t.prev[T])->out
             }
 
-        lazy val inattr : T => U =
-            attr {
-                case t =>
-                    if (init.isDefinedAt (t))
-                        init (t)
-                    else
-                        indflt (t)
-            }
-            
+        lazy val in : T => U =
+            update (indflt, inupdate)
+
         lazy val outdflt : T => U =
             attr {
                 case t if t.hasChildren =>
-                    (t.lastChild[T])->outattr
+                    (t.lastChild[T])->out
                 case t =>
-                    t->inattr
+                    t->in
             }
 
-        lazy val out = update (outdflt)
+        lazy val out : T => U =
+            update (outdflt, outupdate)
 
-        lazy val outattr : T => U =
-            attr {
-                case t =>
-                    if (out.isDefinedAt (t))
-                        out (t)
-                    else
-                        outdflt (t)
-            }
-                  
-        (inattr, outattr)
-
+        Chain (in, out)
     }
 
 }
