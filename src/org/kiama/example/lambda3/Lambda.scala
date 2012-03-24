ef6d58e702770b0c97df3cbdae1003d4cd0575db
Tony Sloane <inkytonik@gmail.com>
2012-03-24 09:02:20 +0100

    Add nominal rewriting support and example

---
+++ src/org/kiama/example/lambda3/Lambda.scala
@@ -0,0 +1,241 @@
+/*
+ * This file is part of Kiama.
+ *
+ * Copyright (C) 2012 Anthony M Sloane, Macquarie University.
+ *
+ * Kiama is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Kiama is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kiama.  (See files COPYING and COPYING.LESSER.)  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiama
+
+/**
+ * Simple lambda calculus implementation to illustrate Kiama's support for
+ * nominal rewriting. This implementation is closely based on the example
+ * used in Scrap your Nameplate, James Cheney, ICFP 2005.
+ */
+package example.lambda3
+
+import org.kiama.util.ParsingREPL
+
+/**
+ * A simple lambda calculus using abstracted name binding.
+ * The basic term syntax is augmented with query commands for the REPL.
+ */
+object AST {
+
+    import Evaluator.cbn_eval
+    import org.kiama.rewriting.NominalAST.{Bind, Name}
+    import org.kiama.rewriting.NominalRewriter.{alphaequiv, fresh, fv,
+        subst, swap, Trans}
+
+    /**
+     * Lambda calculus expressions.
+     */
+    abstract class Exp
+
+    /**
+     * Numeric expression.
+     */
+    case class Num (i : Int) extends Exp {
+        override def toString () = i.toString
+    }
+
+    /**
+     * Variable expression.
+     */
+    case class Var (x : Name) extends Exp {
+        override def toString () = x.toString
+    }
+
+    /**
+     * Application of l to r.
+     */
+    case class App (e1 : Exp, e2 : Exp) extends Exp {
+        override def toString () = "(" + e1 + " " + e2 + ")"
+    }
+
+    /**
+     * Lambda expression containing an abstracted binding.
+     */
+    case class Lam (b : Bind) extends Exp {
+        override def toString () = "(\\" + b.name + " . " + b.term + ")"
+    }
+
+    /**
+     * A query that can be entered from the REPL and returns a value of
+     * type T when executed. These values are not in the term language but
+     * are used to represent user commands.
+     */
+    abstract class Query {
+        type T
+        def execute () : T
+    }
+
+    /**
+     * A query that determines the alpha equivalence of two expressions.
+     */
+    case class EquivQuery (e1 : Exp, e2 : Exp) extends Query {
+        type T = Boolean
+        def execute () = alphaequiv (e1, e2)
+    }
+
+    /**
+     * A query that computes the value of an expression.
+     */
+    case class EvalQuery (e : Exp) extends Query {
+        type T = Exp
+        def execute () = cbn_eval (e)
+    }
+
+    /**
+     * A query that determines the free names in an expression.
+     */
+    case class FreeNamesQuery (e : Exp) extends Query {
+        type T = Set[Name]
+        def execute () = fv (e)
+    }
+    
+    /**
+     * A query that determines whether a name is not free in an expression.
+     */
+    case class FreshQuery (n : Name, e : Exp) extends Query {
+        type T = Boolean
+        def execute () = fresh (n) (e)
+    }
+
+    /**
+     * A query that substitutes an expression `e1` for name `n` in another
+     * expression `e2`.
+     */
+    case class SubstQuery (n : Name, e1 : Exp, e2 : Exp) extends Query {
+        type T = Exp
+        def execute () = subst (n, e1) (e2)
+    }
+
+    /**
+     * A query that swaps two names in an expression.
+     */
+    case class SwapQuery (tr : Trans, e : Exp) extends Query {
+        type T = Exp
+        def execute () = swap (tr) (e)
+    }
+
+}
+
+/**
+ * Parser for simple lambda calculus plus REPL queries.
+ */
+ trait Parser extends org.kiama.util.ParserUtilities {
+
+    import AST._
+    import org.kiama.rewriting.NominalAST.{Bind, Name}
+    import org.kiama.rewriting.NominalRewriter.Trans
+
+    lazy val start =
+        phrase (query)
+
+    lazy val query : PackratParser[Query] =
+        exp ~ ("===" ~> exp) ^^ EquivQuery |
+        ("fv" ~> exp) ^^ FreeNamesQuery |
+        name ~ ("#" ~> exp) ^^ FreshQuery |
+        ("[" ~> name) ~ ("-> " ~> exp <~ "]") ~ exp ^^ SubstQuery |
+        trans ~ exp ^^ SwapQuery |
+        exp ^^ EvalQuery
+
+    lazy val trans : PackratParser[Trans] =
+        "(" ~> name ~ ("<->" ~> name) <~ ")"
+
+    lazy val exp : PackratParser[Exp] =
+        exp ~ factor ^^ App |
+        ("\\" ~> name) ~ ("." ~> exp) ^^ {
+            case n ~ e => Lam (Bind (n, e))
+        } |
+        factor |
+        failure ("expression expected")
+
+    lazy val factor : PackratParser[Exp] =
+        integer | variable | "(" ~> exp <~ ")"
+
+    lazy val integer =
+        "[0-9]+".r ^^ (s => Num (s.toInt))
+
+    lazy val variable =
+        name ^^ Var
+
+    lazy val name =
+        "[a-zA-Z]+".r ~ regexnows ("[0-9]*".r) ^^ {
+            case base ~ index =>
+                Name (base, if (index isEmpty) None else Some (index.toInt))
+        }
+
+}
+
+/**
+ * Evaluation methods for simple lambda calculus.
+ */
+object Evaluator {
+
+    import AST._
+    import org.kiama.rewriting.NominalAST.Bind
+    import org.kiama.rewriting.NominalRewriter.subst
+
+    /**
+     * Call-by-name evaluation.
+     */
+    def cbn_eval (e : Exp) : Exp =
+        e match {
+            case App (t1, t2) =>
+                val w = cbn_eval (t1)
+                w match {
+                    case Lam (Bind (a, u : Exp)) =>
+                        val v = subst (a, t2) (u)
+                        cbn_eval (v)
+                    case _ =>
+                        App (w, t2)
+                }
+            case _ =>
+                e
+        }
+
+}
+
+/**
+ * A read-execute-print loop for lambda calculus queries.
+ */
+object Lambda extends ParsingREPL[AST.Query] with Parser {
+
+    override def setup (args : Array[String]) : Boolean = {
+        println
+        println ("Enter lambda calculus queries:")
+        println
+        println (" e               evaluate e")
+        println (" (n1 <-> n2) e   swap n1 and n2 in e")
+        println (" n # e           is n fresh in e?")
+        println (" fv e            free variables of e")
+        println (" e1 === e2       is e1 alpha equivalent to e2?")
+        println (" [n -> e1] e2    substitute e1 for n in e2")
+        println
+        println ("where n = name, e = expression")
+        println
+        true
+    }
+
+    override def prompt () = "query> "
+
+    def process (q : AST.Query) {
+        println (q.execute ())
+    }
+
+}
---
+++ src/org/kiama/example/lambda3/LambdaTests.scala
@@ -0,0 +1,192 @@
+/*
+ * This file is part of Kiama.
+ *
+ * Copyright (C) 2012 Anthony M Sloane, Macquarie University.
+ *
+ * Kiama is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Kiama is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kiama.  (See files COPYING and COPYING.LESSER.)  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiama
+package example.lambda3
+
+import org.junit.runner.RunWith
+import org.kiama.util.Tests
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.prop.Checkers
+
+/**
+ * Simple lambda calculus query tests.
+ */
+@RunWith(classOf[JUnitRunner])
+class LambdaTests extends Tests with Parser {
+
+    import AST._
+    import org.kiama.rewriting.NominalAST.GenName
+
+    /**
+     * Parse and evaluate str as a query then evaluate the query and compare
+     * the outcome to result. Fail if any of the parsing or the comparison
+     * fail. `T` is the result type of the query. Generated name counting 
+     * is reset before the test is run.
+     */
+    def expectQuery[T] (str : String, result : T) {
+        GenName.reset ()
+        parseAll (start, str) match {
+            case Success (query, in) if in.atEnd =>
+                expect (result) (query.execute ())
+            case Success (_, in) =>
+                fail ("extraneous input at " + in.pos + ": " + str)
+            case f =>
+                fail ("parse failure: " + f)
+        }
+    }
+
+    /**
+     * As for expectQuery except that the expected result is the `toString`
+     * of the query result.
+     */
+    def expectQueryPrint[T] (str : String, result : String) {
+        GenName.reset ()
+        parseAll (start, str) match {
+            case Success (query, in) if in.atEnd =>
+                expect (result) (query.execute ().toString)
+            case Success (_, in) =>
+                fail ("extraneous input at " + in.pos + ": " + str)
+            case f =>
+                fail ("parse failure: " + f)
+        }
+    }
+
+    /**
+     * As for expectQuery except that the expected result is a string
+     * which is first parsed as an expression to obtain the expected result
+     * value.
+     */
+    def expectQueryParse (str : String, result : String) {
+        parseAll (exp, result) match {
+            case Success (resultexp, in) if in.atEnd =>
+                expectQuery (str, resultexp)
+            case Success (_, in) =>
+                fail ("extraneous input in expected result at " + in.pos + ": " + result)
+            case f =>
+                fail ("parse failure in expected result: " + f)
+        }
+    }
+
+    // Test construction short-hands
+
+    def mkvaluetest[T] (s : String, r : T) {
+        test (s + " is " + r) {
+            expectQuery (s, r)
+        }
+    }
+
+    def mkprinttest (s : String, r : String) {
+        test (s + " is " + r) {
+            expectQueryPrint (s, r)
+        }
+    }
+
+    def mkparsetest (s : String, r : String) {
+        test (s + " is " + r) {
+            expectQueryParse (s, r)
+        }
+    }
+
+    // Freshness
+
+    mkvaluetest ("e # 1", true)
+    mkvaluetest ("e # e", false)
+    mkvaluetest ("e # f", true)
+    mkvaluetest ("e # (\\e . e)", true)
+    mkvaluetest ("e # (\\f . f)", true)
+    mkvaluetest ("e # (\\f . e)", false)
+    mkvaluetest ("e # (\\f . f) (\\g . g)", true)
+    mkvaluetest ("e # (\\f . f) (\\e . e)", true)
+    mkvaluetest ("e # (\\f . f) (\\g . e)", false)
+
+    // Swapping
+
+    mkparsetest ("(a <-> a) a", "a")
+    mkparsetest ("(a <-> a) b", "b")
+    mkparsetest ("(a <-> b) a", "b")
+    mkparsetest ("(a <-> b) b", "a")
+    mkparsetest ("(a <-> b) c", "c")
+    mkparsetest ("(a <-> b) a b", "b a")
+    mkparsetest ("(a <-> b) a c", "b c")
+    mkparsetest ("(a <-> b) c b", "c a")
+    mkparsetest ("(a <-> b) \\x . x", "\\x . x")
+    mkparsetest ("(a <-> b) \\a . a", "\\b . b")
+    mkparsetest ("(a <-> b) \\b . b", "\\a . a")
+    mkparsetest ("(a <-> b) (\\b . b a) (\\x . x)", "(\\a . a b) (\\x . x)")
+    mkparsetest ("(a <-> b) (\\a . b) (\\b . a)", "(\\b . a) (\\a . b)")
+
+    // Alpha equivalence
+
+    mkvaluetest ("a === a", true)
+    mkvaluetest ("a === b", false)
+    mkvaluetest ("a === \\x . x", false)
+    mkvaluetest ("a === b c", false)
+    mkvaluetest ("\\a . a === \\b . b", true)
+    mkvaluetest ("\\a . a === \\b . a", false)
+    mkvaluetest ("\\a . a === a b", false)
+    mkvaluetest ("\\a . \\a . a === \\b . \\a . b", false)
+    mkvaluetest ("\\a . \\a . a === \\a . \\a . b", false)
+    mkvaluetest ("\\a . \\a . a === \\b . \\a . a", true)
+    mkvaluetest ("(\\x . x) (\\y. y) === (\\a . a) (\\b . b)", true)
+    mkvaluetest ("(\\x . x) (\\y. y) === (\\x . x) (\\b . b)", true)
+    mkvaluetest ("(\\x . x) (\\y. y) === (\\x . y) (\\y . x)", false)
+
+    // Substitution
+
+    mkparsetest ("[a -> 1] a", "1")
+    mkparsetest ("[a -> 1] b", "b")
+    mkparsetest ("[a -> 1] \\x . x", "\\x0 . x0")
+    mkparsetest ("[a -> 1] \\x . a", "\\x0 . 1")
+    mkparsetest ("[a -> b] \\a . a", "\\a0 . a0")
+    mkparsetest ("[a -> b] \\b . a", "\\b0 . b")
+    mkparsetest ("[a -> b] \\b . b a", "\\b0 . b0 b")
+    mkparsetest ("[a -> b] \\b . \\a . a", "\\b0 . \\a1 . a1")
+
+    // Free variables
+
+    mkprinttest ("fv 1", "Set()")
+    mkprinttest ("fv a", "Set(a)")
+    mkprinttest ("fv a b", "Set(a, b)")
+    mkprinttest ("fv \\a . a", "Set()")
+    mkprinttest ("fv \\a . b", "Set(b)")
+    mkprinttest ("fv \\a . a b", "Set(b)")
+    mkprinttest ("fv \\a . b a", "Set(b)")
+    mkprinttest ("fv \\a . b c", "Set(b, c)")
+    mkprinttest ("fv \\a . \\b . a", "Set()")
+    mkprinttest ("fv \\a . \\b . b", "Set()")
+    mkprinttest ("fv \\a . \\b . c", "Set(c)")
+
+    // Evaluation
+
+    mkparsetest ("1", "1")
+    mkparsetest ("a", "a")
+    mkparsetest ("a b", "a b")
+    mkparsetest ("a (\\b . b)", "a (\\b . b)")
+    mkparsetest ("a (\\b . b) 1", "(a (\\b . b)) 1")
+    mkparsetest ("a ((\\b . b) 1)", "a ((\\b . b) 1)")
+    mkparsetest ("(\\a . a) 1", "1")
+    mkparsetest ("(\\a . a) a", "a")
+    mkparsetest ("(\\a . a) b", "b")
+    mkparsetest ("(\\a . a) (\\b . b) a", "a")
+    mkparsetest ("(\\a . a c) (\\b . b)", "c")
+    mkparsetest ("(\\a . \\b . a b) b", "\\b0 . b b0")
+
+}
