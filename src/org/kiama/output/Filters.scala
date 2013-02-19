a452c046cac29b01e271c73c09c1d2f4fbc12937
Tony Sloane <inkytonik@gmail.com>
2013-02-19 15:34:26 +1100

    Add first set of output filters designed to post-process pretty-printer output

---
+++ src/org/kiama/output/Filters.scala
@@ -0,0 +1,98 @@
+/*
+ * This file is part of Kiama.
+ *
+ * Copyright (C) 2013 Anthony M Sloane, Macquarie University.
+ *
+ * Kiama is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Kiama is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kiama.  (See files COPYING and COPYING.LESSER.)  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiama
+package output
+
+/**
+ * A collection of useful string filters. They are particularly intended
+ * to be filters for pretty-printer output so that the output can be
+ * tailored for a restricted setting in which it will be put. E.g., a
+ * program might be pretty-printed to show in a GUI window of a particular
+ * size, or lines indented greater than a certain amount might be omitted
+ * to show an overview.
+ */
+ trait Filters {
+
+    /**
+     * A filter that limits the string `s` to at most `n` characters.
+     */
+    def keepMaxChars (n : Int) (s : String) : String =
+        s.take (n)
+
+    /**
+     * A filter that limits the string `s` to at most `n` completed
+     * lines. The final end of line is included.
+     */
+    def keepMaxLines (n : Int) (s : String) : String =
+        s.linesWithSeparators.take (n).mkString
+
+    /**
+     * A filter that limits the string `s` to at most `n` words. A word
+     * is one or more consecutive non-whitespace characters. The
+     * whitespace after the last word (if any) is not included.
+     */
+    def keepMaxWords (n : Int) (s : String) : String = {
+        val wordRE = """^(?:\s*[^\s]+){0,%d}""".format (n).r
+        wordRE.findFirstIn (s).getOrElse ("")
+    }
+
+    /**
+     * A replacement function that when given an integer `n` returns the
+     * string `"..."` preceded by `n` spaces. The string argument `s` is
+     * ignored.
+     */
+    def indentedEllipsis (n : Int, s : String) : String =
+        (" " * n) + "...\n"
+
+    /**
+     * Return the indentation of a line, i.e., the number of spaces that
+     * appear before the first non-space character.
+     */
+    def indentOf (s : String) : Int =
+        s.takeWhile (_.isSpaceChar).length
+
+    /**
+     * A filter that replaces runs of lines that have an indentation
+     * level of at least `n` spaces. A run of replaced lines will be
+     * replaced by the result of a call `mkrepl (n, l)` where `l` is
+     * the first line of the run. By default, `mkrepl` is
+     * `indentedEllipsis`.
+     */
+    def keepMaxIndent (n : Int, s : String,
+                       mkrepl : (Int, String) => String = indentedEllipsis) : String = {
+        s.linesWithSeparators.toList.foldLeft ((List[String] (), true)) {
+            case ((result, first), l) =>
+                if (indentOf (l) >= n)
+                    if (first)
+                        (result :+ mkrepl (n, l), false)
+                    else
+                        (result, false)
+                else
+                    (result :+ l, true)
+        }._1.mkString
+    }
+
+}
+
+/**
+ * A collection of useful string filters.
+ */
+object Filters extends Filters
---
+++ src/org/kiama/output/FiltersTests.scala
@@ -0,0 +1,202 @@
+/*
+ * This file is part of Kiama.
+ *
+ * Copyright (C) 2013 Anthony M Sloane, Macquarie University.
+ *
+ * Kiama is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Kiama is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kiama.  (See files COPYING and COPYING.LESSER.)  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiama
+package output
+
+import org.kiama.util.Tests
+import org.scalatest.prop.Checkers
+
+/**
+ * Basic tests of filters.
+ */
+class FiltersTests extends Tests with Checkers with PrettyPrinter {
+
+    import Filters.{keepMaxChars, keepMaxIndent, keepMaxLines,
+        keepMaxWords}
+    import org.scalacheck.Prop._
+
+    test ("keepMaxChars can handle empty string") {
+        check ((i : Int) => (i >= 0) ==> (keepMaxChars (i) ("") == ""))
+    }
+
+    test ("keepMaxLines can handle empty string") {
+        check ((i : Int) => (i >= 0) ==> (keepMaxLines (i) ("") == ""))
+    }
+
+    test ("keepMaxWords can handle empty string") {
+        check ((i : Int) => (i >= 0) ==> (keepMaxWords (i) ("") == ""))
+    }
+
+    {
+        val output =
+            """The first line
+            |  the second, line
+            |    the third line!
+            |:the final line
+            |""".stripMargin
+
+        test ("keepMaxChars can handle zero count") {
+            expectResult ("") (keepMaxChars (0) (output))
+        }
+
+        test ("keepMaxChars can handle count in first line") {
+            expectResult ("The firs") (keepMaxChars (8) (output))
+        }
+
+        test ("keepMaxChars can handle count in an inner line") {
+            expectResult ("The first line\n  the s") (keepMaxChars (22) (output))
+        }
+
+        test ("keepMaxChars can handle count after end") {
+            expectResult (output) (keepMaxChars (200) (output))
+        }
+
+        test ("keepMaxLines can handle zero count") {
+            expectResult ("") (keepMaxLines (0) (output))
+        }
+
+        test ("keepMaxLines can handle one count") {
+            expectResult ("The first line\n") (keepMaxLines (1) (output))
+        }
+
+        test ("keepMaxLines can handle an inner count") {
+            expectResult ("The first line\n  the second, line\n    the third line!\n") (
+                keepMaxLines (3) (output)
+            )
+        }
+
+        test ("keepMaxLines can handle count after end") {
+            expectResult (output) (keepMaxLines (10) (output))
+        }
+
+        test ("keepMaxWords can handle zero count") {
+            expectResult ("") (keepMaxWords (0) (output))
+        }
+
+        test ("keepMaxWords can handle count in first line") {
+            expectResult ("The first") (keepMaxWords (2) (output))
+        }
+
+        test ("keepMaxWords can handle count in an inner line") {
+            expectResult ("The first line\n  the second, line\n    the third") (
+                keepMaxWords (8) (output)
+            )
+        }
+
+        test ("keepMaxWords can handle count after end") {
+            expectResult (output.init) (keepMaxWords (20) (output))
+        }
+
+    }
+
+    {
+        val output =
+            """  The first line
+            |the second line
+            |  the third line
+            |  the fourth line
+            |    the fifth line
+            |    the sixth line
+            |  the seventh line
+            |  the eighth line
+            |the ninth line
+            |    the tenth line
+            |""".stripMargin
+
+        test ("keepMaxIndent with indent of zero replaces all") {
+            expectResult ("...\n") (keepMaxIndent (0, output))
+        }
+
+        test ("keepMaxIndent with indent of one keeps just top-level lines") {
+            val result =
+                """ ...
+                |the second line
+                | ...
+                |the ninth line
+                | ...
+                |""".stripMargin
+            expectResult (result) (keepMaxIndent (1, output))
+        }
+
+        test ("keepMaxIndent with indent of two keeps just top-level lines") {
+            val result =
+                """  ...
+                |the second line
+                |  ...
+                |the ninth line
+                |  ...
+                |""".stripMargin
+            expectResult (result) (keepMaxIndent (2, output))
+        }
+
+        test ("keepMaxIndent with indent of two and FIXME insertion works") {
+
+            def fixmeInsertion (n : Int, s : String) : String =
+                ("FIXME" * n) + "\n"
+
+            val result =
+                """FIXMEFIXME
+                |the second line
+                |FIXMEFIXME
+                |the ninth line
+                |FIXMEFIXME
+                |""".stripMargin
+            expectResult (result) (keepMaxIndent (2, output, fixmeInsertion))
+
+        }
+
+        test ("keepMaxIndent with indent of three keeps top two levels") {
+            val result =
+                """  The first line
+                |the second line
+                |  the third line
+                |  the fourth line
+                |   ...
+                |  the seventh line
+                |  the eighth line
+                |the ninth line
+                |   ...
+                |""".stripMargin
+            expectResult (result) (keepMaxIndent (3, output))
+        }
+
+        test ("keepMaxIndent with indent of four keeps top two levels") {
+            val result =
+                """  The first line
+                |the second line
+                |  the third line
+                |  the fourth line
+                |    ...
+                |  the seventh line
+                |  the eighth line
+                |the ninth line
+                |    ...
+                |""".stripMargin
+            expectResult (result) (keepMaxIndent (4, output))
+        }
+
+        test ("keepMaxIndent with indent of five keeps everything") {
+            expectResult (output) (keepMaxIndent (5, output))
+        }
+
+    }
+
+}
