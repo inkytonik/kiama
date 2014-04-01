af0d4eefcb674fe9fb45fbfc846eada69aa28960
Tony Sloane <inkytonik@gmail.com>
2014-04-01 11:10:51 +1100

    Move from Stacks to Lists in Envrionments module

--- library/src/org/kiama/util/Environments.scala
+++ library/src/org/kiama/util/Environments.scala
@@ -22,7 +22,6 @@ package org.kiama
 package util
 
 import scala.collection.immutable
-import scala.collection.immutable.Stack
 
 /**
  * General implementation of environments as stacked scopes.  The objects
@@ -83,49 +82,49 @@ trait Environments {
     /**
      * An environment is a stack of scopes with the innermost scope on the top.
      */
-    type Environment = Stack[Scope]
+    type Environment = List[Scope]
 
     /**
      * Create a root environment, i.e., one that has a single scope containing
      * the given bindings.
      */
     def rootenv (bindings : (String,Entity)*) : Environment =
-        Stack (immutable.HashMap (bindings : _*))
+        List (immutable.HashMap (bindings : _*))
 
     /**
      * Enter a new empty scope nested within the given environment.
      */
     def enter (env : Environment) : Environment =
-        env.push (new immutable.HashMap[String,Entity])
+        (new immutable.HashMap[String,Entity]) :: env
 
     /**
      * Leave the outermost scope of the given environment, raising an error if
      * the environment is empty.
      */
     def leave (env : Environment) : Environment =
-        if (env.isEmpty)
-            sys.error ("leave called on empty environment")
-        else
-            env.pop
+        env match {
+            case _ :: t => t
+            case _      => sys.error ("leave called on empty environment")
+        }
 
     /**
      * Define i to be e in the current scope of env, raising an error if the
      * environment is empty.
      */
-    def define (env : Environment, i : String, e : Entity) : Environment = {
-        if (env.isEmpty)
-            sys.error ("define called on empty environment")
-        else {
-            val s = env.top
-            env.pop.push (s + ((i, e)))
+    def define (env : Environment, i : String, e : Entity) : Environment =
+        env match {
+            case s :: t => (s + ((i, e))) :: t
+            case _      => sys.error ("define called on empty environment")
         }
-    }
 
     /**
      * Say whether i is defined in the current scope of env.
      */
     def isDefinedInScope (env : Environment, i : String) : Boolean =
-        env.nonEmpty && ((env.top) contains i)
+        env match {
+            case s :: _ => s contains i
+            case _      => false
+        }
 
     /**
      * Say whether i is defined in the given scope.
@@ -144,14 +143,20 @@ trait Environments {
      * current scope).
      */
     def isDefinedInInner (env : Environment, i : String) : Boolean =
-        isDefinedInScope (env.top, i)
+        env match {
+            case s :: _ => isDefinedInScope (s, i)
+            case _      => false
+        }
 
     /**
      * Say whether i is defined in an outer scope of env (i.e., not in the
      * current scope).
      */
     def isDefinedInOuter (env : Environment, i : String) : Boolean =
-        isDefinedInEnv (env.pop, i)
+        env match {
+            case _ :: t => isDefinedInEnv (t, i)
+            case _      => false
+        }
 
     /**
      * Look up i in env, returning the mapped Entity if there is one, otherwise
@@ -159,13 +164,16 @@ trait Environments {
      * search outwards in all scopes, returning the first Entity found, if any.
      */
     def lookup (env : Environment, i : String, e : Entity, scope : Boolean = false) : Entity =
-        if (env.isEmpty)
-            e
-        else if (scope)
-            env.top.getOrElse (i, e)
-        else if (isDefinedInScope (env, i))
-            env.top (i)
-        else
-            lookup (env.pop, i, e)
+        env match {
+            case s :: t =>
+                if (scope)
+                    s.getOrElse (i, e)
+                else if (isDefinedInScope (env, i))
+                    s (i)
+                else
+                    lookup (t, i, e)
+            case _ =>
+                e
+        }
 
 }
