7a32a407eca9df9e75f3e74b749a60a358dd75f6
Tony Sloane <inkytonik@gmail.com>
2016-12-16 10:27:15 +1100

    Reduce relation footprint by removing unused or shakily-defined operations
    The ops are collect, compose, index, preImage, preIndex, projDomain, projRange,
    union, withDimain and withRange. Most of them were poorly motivated and are
    not used in the new implementation. index had shaky semantics since order is
    not really a relational thing. We still have index at the tree level which has a much
    sounder basis.

--- library/src/main/scala/org/bitbucket/inkytonik/kiama/relation/RelationLike.scala
+++ library/src/main/scala/org/bitbucket/inkytonik/kiama/relation/RelationLike.scala
@@ -50,22 +50,6 @@ trait RelationLike[T, U, Repr[_, _]] {
     def apply(t : T) : Vector[U] =
         image(t)
 
-    // /**
-    //  * Build a new relation by collecting pairs produced by the partial
-    //  * function `f` wherever it is defined on pairs of this relation.
-    //  */
-    // def collect[V, W](f : ((T, U)) ==> (V, W)) : Repr[V, W] =
-    //     companion.fromGraph(graph.collect(f))
-    //
-    // /**
-    //  * Compose this relation with `st`.
-    //  */
-    // def compose[S](st : RelationLike[S, T, Repr]) : Repr[S, U] =
-    //     companion.fromGraph(
-    //         for ((s, t1) <- st.graph; (t2, u) <- graph; if same(t1, t2))
-    //             yield (s, u)
-    //     )
-
     /**
      * Does the domain of this relation contain the value `t`?
      */
@@ -90,13 +74,6 @@ trait RelationLike[T, U, Repr[_, _]] {
      */
     def image(t : T) : Vector[U] =
         graph.image(t)
-
-    // /**
-    //  * A relation that maps each element of the range to its position
-    //  * (starting counting at zero).
-    //  */
-    // lazy val index : Repr[U, Int] =
-    //     companion.fromGraph(graph.map(_._2).zipWithIndex)
 
     /**
      * Invert this relation. In other words, if `(t,u)` is in the relation,
@@ -123,34 +100,6 @@ trait RelationLike[T, U, Repr[_, _]] {
 
     }
 
-    // /**
-    //  * The preImage of a value of the relation's range is a set of the
-    //  * values in the domain that are related to that range value.
-    //  */
-    // def preImage(u : U) : Vector[T] =
-    //     inverse.image(u)
-    //
-    // /**
-    //  * A relation that maps each element of the domain to its position
-    //  * starting at zero.
-    //  */
-    // lazy val preIndex : Repr[T, Int] =
-    //     companion.fromGraph(graph.map(_._1).zipWithIndex)
-    //
-    // /**
-    //  * Domain projection, i.e., form a relation that relates each
-    //  * value in the domain to all of the related values in the range.
-    //  */
-    // lazy val projDomain : Repr[T, Vector[U]] =
-    //     companion.fromGraph(domain.map(t => (t, image(t))))
-    //
-    // /**
-    //  * Range projection, i.e., form a relation that relates each
-    //  * value in the range to all of the related values in the domain.
-    //  */
-    // lazy val projRange : Repr[U, Vector[T]] =
-    //     companion.fromGraph(range.map(u => (u, preImage(u))))
-
     /**
      * The range of this relation.
      */
@@ -167,25 +116,5 @@ trait RelationLike[T, U, Repr[_, _]] {
      */
     def unapplySeq(t : T) : Option[Vector[U]] =
         Some(image(t))
-
-    // /**
-    //  * Union this relation with `r`.
-    //  */
-    // def union(r : RelationLike[T, U, Repr]) : Repr[T, U] =
-    //     companion.fromGraph(graph ++ r.graph)
-    //
-    // /**
-    //  * Return the sub-relation of this relation that contains just those
-    //  * pairs that have `t` as their domain element.
-    //  */
-    // def withDomain(t : T) : Repr[T, U] =
-    //     companion.fromGraph(graph.filter { case (t1, _) => same(t, t1) })
-    //
-    // /**
-    //  * Return the sub-relation of this relation that contains just those
-    //  * pairs that have `u` as their range element.
-    //  */
-    // def withRange(u : U) : Repr[T, U] =
-    //     companion.fromGraph(graph.filter { case (_, u1) => same(u, u1) })
 
 }
