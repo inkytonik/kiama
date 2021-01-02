3ce8bcf9333a9b20864b10ad51e5843da740ac77
Tony Sloane <tony.sloane@me.com>
2021-01-02 13:05:13 +1100

    Update copyright date

--- extras/src/test/scala/org/bitbucket/inkytonik/kiama/example/oberon0/L3/c/CPrettyPrinter.scala
+++ extras/src/test/scala/org/bitbucket/inkytonik/kiama/example/oberon0/L3/c/CPrettyPrinter.scala
@@ -1,7 +1,7 @@
 /*
  * This file is part of Kiama.
  *
- * Copyright (C) 2011-2020 Anthony M Sloane, Macquarie University.
+ * Copyright (C) 2011-2021 Anthony M Sloane, Macquarie University.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
